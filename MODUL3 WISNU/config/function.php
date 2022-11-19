6bd45b762dffb8f3923e863fef2ccb2df2e3eeab
wisnuwirayuda15 <wisnuwirayuda15@gmail.com>
2022-11-19 12:39:43 +0700

    First Commit

--- MODUL3 WISNU/config/function.php
+++ MODUL3 WISNU/config/function.php
@@ -44,32 +44,4 @@ function upload_gambar()
 
     return $nama_foto_baru;
 }
-
-function update_gambar($nama_foto, $lokasi_foto, $error_foto)
-{
-    if ($error_foto == 4){
-        return false;
-    }
-
-    $extensi_foto_valid = ['jpg', 'jpeg', 'png', 'gif'];
-    $extensi_foto = explode('.', $nama_foto);
-    $extensi_foto = strtolower(end($extensi_foto));
-
-    if (!in_array($extensi_foto, $extensi_foto_valid)) {
-        echo "
-        <script>
-            alert('File yang diupload tidak didukung!')
-        </script>";
-        header("Location: ../index.php");
-        exit;
-    }
-
-    $nama_foto_baru = uniqid();
-    $nama_foto_baru .= '.';
-    $nama_foto_baru .= $extensi_foto;
-
-    move_uploaded_file($lokasi_foto, '../assets/img/upload/'.$nama_foto_baru);
-
-    return $nama_foto_baru;
-}
 ?>
